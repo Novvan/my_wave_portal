89df2ae41d886f89436611ada24051e5ef36aa6e
NoVvaN <ianallengeier@gmail.com>
2021-11-23 18:36:51 -0300

    update

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -12,20 +12,41 @@ contract WavePortal {
 
     event NewPlaylist(address indexed from, uint256 timestamp, string url);
 
-    Playlist[] playlists;
-    mapping(address => uint256) public uploads;
+    uint256 private seed;
 
-    constructor() {
+    Playlist[] playlists;
+    mapping(address => uint256) public lastUpload;
+
+    constructor() payable {
         console.log("Pepe is hereeeeee!");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function uploadPlaylist(string calldata _url) public {
+        require(
+            lastUpload[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+        lastUpload[msg.sender] = block.timestamp;
+
         console.log("%s has uploaded an awesome playlist!", msg.sender);
         _savePlaylist(_url);
     }
 
     function _savePlaylist(string memory _url) private {
-        playlists.push(Playlist(msg.sender,_url, block.timestamp));
+        playlists.push(Playlist(msg.sender, _url, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (seed <= 50) {
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
 
         emit NewPlaylist(msg.sender, block.timestamp, _url);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,29 +1,49 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
   let waveCount;
   waveCount = await waveContract.getTotalUploads();
 
-  let waveTxn = await waveContract.uploadPlaylist("https://open.spotify.com/playlist/37i9dQZF1DXbbu94YBG7Ye?si=e02ebc4e03c14ae7");
-  await waveTxn.wait();
+  console.log("TotalUploads:", waveCount);
+
+  for (let i = 0; i < 5; i++) {
+    let waveTxn = await waveContract.uploadPlaylist(
+      "https://open.spotify.com/playlist/37i9dQZF1DXbbu94YBG7Ye?si=e02ebc4e03c14ae7"
+    );
+    await waveTxn.wait();
+    contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  }
 
   waveCount = await waveContract.getTotalUploads();
 
-  waveTxn = await waveContract.connect(randomPerson).uploadPlaylist("https://open.spotify.com/playlist/37i9dQZF1DXbbu94YBG7Ye?si=e02ebc4e03c14ae7");
-  await waveTxn.wait();
-
-  waveCount = await waveContract.getTotalUploads();
+  console.log("TotalUploads:", waveCount);
 
   let playlists;
   playlists = await waveContract.getPlaylists();
 
-  console.log(playlists);
+  // console.log(playlists);
 };
 
 const runMain = async () => {
