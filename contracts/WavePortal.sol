be38750cfdfcda5c45dd0b4e6b91a8e452ec0a65
NoVvaN <ianallengeier@gmail.com>
2021-11-23 17:59:56 -0300

    UPDATE

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,30 +4,38 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    string[] playlists;
-    mapping(address => uint) public uploads;
+    struct Playlist {
+        address sender;
+        string url;
+        uint256 timestamp;
+    }
+
+    event NewPlaylist(address indexed from, uint256 timestamp, string url);
+
+    Playlist[] playlists;
+    mapping(address => uint256) public uploads;
 
     constructor() {
         console.log("Pepe is hereeeeee!");
     }
 
-    function uploadPlaylist(string calldata  _url) public {
+    function uploadPlaylist(string calldata _url) public {
         console.log("%s has uploaded an awesome playlist!", msg.sender);
-        uploads[msg.sender] = uploads[msg.sender]++;
         _savePlaylist(_url);
     }
+
     function _savePlaylist(string memory _url) private {
-        playlists.push(_url);
+        playlists.push(Playlist(msg.sender,_url, block.timestamp));
+
+        emit NewPlaylist(msg.sender, block.timestamp, _url);
     }
 
     function getTotalUploads() public view returns (uint256) {
-        console.log("We have %d total playlists uploaded!", playlists.length);
+        // console.log("We have %d total playlists uploaded!", playlists.length);
         return playlists.length;
     }
 
-    function getPlaylists() public view returns (string[] memory) {
-        // console.log();
+    function getPlaylists() public view returns (Playlist[] memory) {
         return playlists;
     }
-
 }
